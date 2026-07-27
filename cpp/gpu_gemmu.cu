7179c0b6b51d5b1bf83790710105ef83a4f85f1a
StellWava <stellanaks16@gmail.com>
2026-07-27 13:19:31 -0400

    gemm

--- cpp/gpu_gemmu.cu
+++ cpp/gpu_gemmu.cu
@@ -3,7 +3,7 @@
 an H100 has 132 SMs, and a Blackwell B200
 GEMM is 2D hence fully exploits the threadblock.
 - using shared memory , test shared memory bank conflict
-- - unified memory,
+- unified memory
 equation:  = sum of pdt of shared tile K + C[{row}][{col}]
 no concurrency and pinned memory here -
 */
@@ -16,20 +16,19 @@ no concurrency and pinned memory here -
 #include <cmath>
 #include <cuda_runtime.h>
 
-std::string get_machine_name() {
-#if defined(_WIN32) || defined(_WIN64)
-    const char* env = std::getenv("COMPUTERNAME");
-#else
-    const char* env = std::getenv("HOSTNAME");
-    if (!env) env = std::getenv("NAME");
-#endif
-    return env ? std::string(env) : "GPU_Machine";
-}
-
 // Define compile-time physical tile dimensions inside the SM SRAM = 1024 threads
 #define TILE_DIM 32
 
-__global__ void gemm_kernel(const float *A, const float *B, float *C, int M, int K, int N, float alpha, float beta){
+__global__ void gemm_kernel(
+    const float *A,
+    const float *B,
+    float *C,
+    int M,
+    int K,
+    int N,
+    float alpha,
+    float beta)
+{
     //allocate the device shared memory array - strategy 2
     __shared__ float tile_A[TILE_DIM][TILE_DIM];
     __shared__ float tile_B[TILE_DIM][TILE_DIM];
@@ -49,10 +48,10 @@ __global__ void gemm_kernel(const float *A, const float *B, float *C, int M, int
         */
         if (row < M && (t * TILE_DIM + threadIdx.x) < K) {
             tile_A[threadIdx.y][threadIdx.x] =
-                A[row * K + (t * TILE_DIM + threadIdx.x)];
-        }
+            A[row * K + (t * TILE_DIM + threadIdx.x)];
+        } 
         else {
-            tile_A[threadIdx.y][threadIdx.x] = 0.0f;
+            tile_A[threadIdx.y][threadIdx.x] = 0.0f; 
         }
 
         //load matrix B into shared memory as well
@@ -62,8 +61,8 @@ __global__ void gemm_kernel(const float *A, const float *B, float *C, int M, int
         */
         if (col < N && (t * TILE_DIM + threadIdx.y) < K) {
             tile_B[threadIdx.y][threadIdx.x] =
-                B[(t * TILE_DIM + threadIdx.y) * N + col];
-        }
+            B[(t * TILE_DIM + threadIdx.y) * N + col];
+        } 
         else {
             tile_B[threadIdx.y][threadIdx.x] = 0.0f;
         }
@@ -75,6 +74,7 @@ __global__ void gemm_kernel(const float *A, const float *B, float *C, int M, int
             sum += tile_A[threadIdx.y][k] * tile_B[k][threadIdx.x];
         }
         __syncthreads();
+
     }
 
     //write final alpha/beta and results back to global memory
@@ -108,75 +108,60 @@ int main()
     size_t size_B = K * N * sizeof(float);
     size_t size_C = M * N * sizeof(float);
 
-    //host mem allocation
-    float *h_A = new float[M * K];
-    float *h_B = new float[K * N];
-    float *h_C = new float[M * N];
+    //Unified Memory allocation
+    float *A;
+    float *B;
+    float *C;
 
-    //initialize matrices
-    for (int i = 0; i < M * K; ++i) h_A[i] = 1.0f;
-    for (int i = 0; i < K * N; ++i) h_B[i] = 0.5f;
-    for (int i = 0; i < M * N; ++i) h_C[i] = 2.0f;
+    checkcuda(cudaMallocManaged(&A, size_A));
+    checkcuda(cudaMallocManaged(&B, size_B));
+    checkcuda(cudaMallocManaged(&C, size_C));
 
-    //device allocation - one pointer per matrix
-    float *d_A;
-    float *d_B;
-    float *d_C;
-
-    cudaMalloc(&d_A, size_A);
-    cudaMalloc(&d_B, size_B);
-    cudaMalloc(&d_C, size_C);
-
-    //host to device transfer
-    cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_C, h_C, size_C, cudaMemcpyHostToDevice);
+    //initialize matrices directly through unified memory
+    for (int i = 0; i < M * K; ++i) A[i] = 1.0f;
+    for (int i = 0; i < K * N; ++i) B[i] = 0.5f;
+    for (int i = 0; i < M * N; ++i) C[i] = 2.0f;
 
     //grid mapping on device
     dim3 blockDim(TILE_DIM, TILE_DIM);
-    dim3 gridDim(
-        (N + TILE_DIM - 1) / TILE_DIM,
-        (M + TILE_DIM - 1) / TILE_DIM
-    );
+    dim3 gridDim((N + TILE_DIM - 1) / TILE_DIM,(M + TILE_DIM - 1) / TILE_DIM);
 
     //launch kernel
-    gemm_kernel<<<gridDim, blockDim>>>(
-        d_A, d_B, d_C, M, N, K, alpha, beta
-    );
+    gemm_kernel<<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
 
-    cudaDeviceSynchronize();
+    checkcuda(cudaGetLastError());
+    checkcuda(cudaDeviceSynchronize());
 
     //Active Timing Block
     const auto start = std::chrono::steady_clock::now();
 
     //launch kernel
-    gemm_kernel<<<gridDim, blockDim>>>(
-        d_A, d_B, d_C, M, N, K, alpha, beta
-    );
+    gemm_kernel<<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
+
+    checkcuda(cudaGetLastError());
 
     //sync gpu
-    cudaDeviceSynchronize();
+    checkcuda(cudaDeviceSynchronize());
 
     //measure time
     const auto end = std::chrono::steady_clock::now();
     const std::chrono::duration<double> elapsed = end - start;
     const double seconds = elapsed.count();
 
-    //result back to host
-    cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost);
+    //No device-to-host copy is required.
+    //After synchronization, C can be read directly by the CPU.
 
     //verify results
     float expected = 1.5f * (K * (1.0f * 0.5f)) + beta * 2.0f;
-    std::cout << "Baseline GEMM done. Results:"<< h_C[0]<< " (Expected: "<< expected<< ")"<< std::endl;
+    std::cout << "Baseline GEMM done. Results:"<< C[0]<< " (Expected: "<< expected<< ")"<< std::endl;
 
     // 7. Calculate Metrics
     const std::size_t num_rows = static_cast<std::size_t>(M) * static_cast<std::size_t>(N);
     constexpr double flops_per_element = 2.0;
-
     const double total_flops = flops_per_element * static_cast<double>(M) * static_cast<double>(N) * static_cast<double>(K);
     constexpr double bytes_per_element = 3.0 * sizeof(double);
     const double total_bytes = static_cast<double>(num_rows) * bytes_per_element;
-    
+
     const double gflops = total_flops / seconds / 1.0e9;
     const double bandwidth_gb_s = total_bytes / seconds / 1.0e9;
     const double arithmetic_intensity = flops_per_element / bytes_per_element;
@@ -184,36 +169,36 @@ int main()
     double checksum = 0.0;
 
     for (std::size_t i = 0; i < num_rows; ++i) {
-        checksum += h_C[i];
+        checksum += C[i];
     }
 
-    std::cout << "CUDA GPU GEMM | Bandwidth: "<< bandwidth_gb_s<< " GB/s | Performance: "<< gflops<< " GFLOP/s\n";
+    std::cout << "CUDA GPU GEMM | Bandwidth: " << bandwidth_gb_s << " GB/s | Performance: " << gflops << " GFLOP/s\n";
 
     // --- CSV MANAGEMENT ---
     std::ifstream check_empty("roofline.csv");
 
-    bool add_header = !check_empty.is_open() ||
+    bool add_header =
+        !check_empty.is_open() ||
         check_empty.peek() ==
             std::ifstream::traits_type::eof();
+
     check_empty.close();
 
     std::ofstream csv("roofline.csv", std::ios::app);
 
     if (add_header) {
-        csv << "machine,dwarf name,AI,bandwidth (GB/s)," "performance (GFLOP/s),threads,checksum\n";
+        csv << "machine,dwarf name,AI,bandwidth (GB/s),"
+               "performance (GFLOP/s),threads,checksum\n";
     }
 
     const char *machine_name = "CUDA GPU";
-    csv << machine_name<< ","<< "GEMM Tile"<< ","<< arithmetic_intensity<< ","<< bandwidth_gb_s<< ","<< gflops<< ","<< "CUDA"<< ","<< checksum<< "\n";
 
-    //free up memory
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
+    csv << machine_name<< "," << "GEMM Tileu" << "," << arithmetic_intensity << "," << bandwidth_gb_s << "," << gflops << "," << "CUDA" << "," << checksum << "\n";
 
-    delete[] h_A;
-    delete[] h_B;
-    delete[] h_C;
+    //free unified memory
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(C);
 
     return 0;
 }
